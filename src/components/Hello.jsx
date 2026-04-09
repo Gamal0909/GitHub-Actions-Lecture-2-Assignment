ee36fad0e9973659159e64fc2a63481c146b2fd9
Gamal0909 <gamalmohammed0909@gmail.com>
2026-04-10 01:52:19 +0200

    Update default greeting to Mustafa

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,4 +1,4 @@
 import React from "react";
-export default function Hello({ name = "amr" }) {
+export default function Hello({ name = "Mustafa" }) {
   return <div data-testid="hello">Hello, {name}!</div>;
 };
