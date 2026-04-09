957c5bb0d42d4283888c9f2f52a877f6d42b98c7
Gamal0909 <gamalmohammed0909@gmail.com>
2026-04-10 01:35:08 +0200

    Change default greeting name to amr

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,4 +1,4 @@
 import React from "react";
-export default function Hello({ name = "World" }) {
+export default function Hello({ name = "amr" }) {
   return <div data-testid="hello">Hello, {name}!</div>;
 };
